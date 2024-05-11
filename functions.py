cd74043385bac6be6c4625d12b145419871e80ee
72abdulrehman <abdulrehmanikram9@gmail.com>
2024-05-11 12:15:03 +0500

    Intial Commit

---
+++ functions.py
@@ -0,0 +1,11 @@
+#FILEPATH = "todox.txt"
+
+def get_todos(filepath="todos.txt"):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_args, filepath="todos.txt"):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_args)
